12bcd0408d45647686228b370d95943a77820cfe
kkravchenko-w <kravchenko.konstantin.work@gmail.com>
2026-01-23 02:05:19 +0300

    Complete task 1 part 1 and 2

---
+++ tasks/kanstantsin-krauchanka/tasks-part-1.js
@@ -0,0 +1,54 @@
+//1
+let firstName = "John";
+let lastName = "Adams";
+console.log(firstName + " " + lastName);
+
+firstName = "Brandon";
+lastName = "Smith";
+console.log(firstName + " " + lastName + "\n");
+
+//2
+let a;
+let c = 9;
+let str = "Hi 5!";
+let b = true;
+let y = 9 + "1";
+let x = "a" / 6;
+
+console.log(typeof(a));
+console.log(typeof(c));
+console.log(typeof(str));
+console.log(typeof(b));
+console.log(typeof(y));
+console.log(typeof(x) + "\n");
+
+//3
+const PI = 3.1415;
+const myName = "Kostya";
+
+console.log(myName + "\n" + PI + "\n");
+
+//4
+for (let i = 10; i < 21; i++) {
+    console.log(Boolean(i));
+};
+
+console.log("\n");
+
+
+//7
+let aa = "100";
+let bb = "25px";
+let cc = "3.14";
+
+console.log(Number(aa));
+console.log(Number(bb));
+console.log(Number(cc));
+console.log();
+console.log(parseInt(aa));
+console.log(parseInt(bb));
+console.log(parseInt(cc));
+console.log();
+console.log(parseFloat(aa));
+console.log(parseFloat(bb));
+console.log(parseFloat(cc));
